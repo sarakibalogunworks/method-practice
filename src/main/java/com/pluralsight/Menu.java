d6755b01d138eb4efe43c1649dcaffd7e6f43f76
sarakibalogunworks <sarakibalogunworks@gmail.com>
2026-04-10 10:19:46 -0400

    done with exercise 2 commit

--- src/main/java/com/pluralsight/Menu.java
+++ src/main/java/com/pluralsight/Menu.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
 public class Menu {
-}
+    public static void main(String[] args) {
+
+        System.out.println("Welcome! Here is our menu: ");
+        displayMenu();
+
+        System.out.println("\nLet me reshow you the menu again: ");
+        displayMenu();
+
+    }
+
+    public static void displayMenu() {
+        System.out.println(" $$ MENU $$");
+
+        System.out.println("1. Coffee - $3.99 ");
+
+        System.out.println("2. Tea - $2.99 ");
+
+        System.out.println("3. Cookie - $1.99 ");
+    }
+
+    }
